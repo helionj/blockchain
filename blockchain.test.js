9fdc8598cbc5d84122b5920a7f241cec2c4db9d5
helionj <helionporto@hotmail.com>
2021-12-21 20:11:22 -0300

    Validation chain and test

--- blockchain.test.js
+++ blockchain.test.js
@@ -3,11 +3,13 @@ const Block = require ('./block');
 
 describe( 'Blockchain',  () => {
 
-    let bc;
+    let bc, bc2;
 
     beforeEach(()=> {
        bc = new Blockchain();
-    })
+       bc2 = new Blockchain();
+    });
+
     it('sets the `data` to match the input', () => {
         expect(bc.chain[0]).toEqual(Block.genesis());
     });
@@ -16,5 +18,24 @@ describe( 'Blockchain',  () => {
         const data = 'foo';
         bc.addBlock(data);
         expect(bc.chain[bc.chain.length-1].data).toEqual(data);
+    });
+
+    it('validates a valid chain', () => {
+        
+        bc2.addBlock('foo');
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with a corrupt genisis block', () => {
+        
+        bc2.chain[0].data = 'Bad block';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupt chain', () => {
+        
+        bc2.addBlock('foo');
+        bc2.chain[1].data = 'Not foo';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
     });
 });
